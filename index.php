57fb96febe994972fcce65a4e907a23813e4cb45
triggo_b <info@admin.ru>
2013-12-18 12:35:49 +0300

    исключения, +игнор папок

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
-<?php
-
-    public function getFactorial($num)
-    {
-        try {
-            if ($num && $num > 0) {
-                if ($num === 0) {
-                    $overall = 1;
-                } else {
-                    $overall = $num;
-                }
-                for ($i = $num; $i > 0; $i --) {
-                    $overall *= $i;
-                }
-                return $overall;
+<?php 
+public function getFactorial($num)
+{
+    try {
+        if ($num && $num > 0) {
+            if ($num === 0) {
+                $overall = 1;
             }
-             else {
-                throw new Exception('неверно');
+            else {
+                $overall = $num;
             }
-        } catch (Exception $e) {
-            echo $e->getMessage();
+            for ($i = $num; $i > 0; $i --) {
+                $overall *= $i;
+            }
+            return $overall;
         }
+        else {
+            throw new Exception('неверно');
+        }
+    }
+    catch (Exception $e) {
+        echo $e->getMessage();
     }
 }
